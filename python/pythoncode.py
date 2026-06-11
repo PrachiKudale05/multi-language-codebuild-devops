e23a2526159dfd45c6ab83fd9f80a0a9c13a3895
Prachi Kudale <kudale.prachi05@gmail.com>
2026-06-11 11:47:40 +0530

    Create pythoncode.py

---
+++ python/pythoncode.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100,7263,237,236,2737,236,379,272,3787,283]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
